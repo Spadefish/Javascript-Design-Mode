f7320970f9ebdb7778bfb198722d2d090589b2ba
spadefish <793378953@qq.com>
2019-10-31 10:39:40 +0800

    基于vue-cli + es6实现一些常用的设计模式

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import 'lib-flexible'
+import App from './App'
+
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
