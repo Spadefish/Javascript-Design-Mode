8ebf6de096d542857529591d7ec532fdfbf6973b
spadefish <793378953@qq.com>
2019-11-06 19:34:05 +0800

    vue 基础api 测试

--- src/views/single-case/index.vue
+++ src/views/single-case/index.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
     <h1>这是单例模式学习</h1>
+    <test-demo ref="testDemo">我是slot哈哈哈哈</test-demo>
+    <div id="test"></div>
   </div>
 </template>
 
 <script>
+import TestDemo from '@/views/single-case/test-demo'
 export default {
+  name: 'single-case',
+  components: {TestDemo},
   data() {
     return {
 
     }
+  },
+  methods: {
+
+  },
+  mounted() {
+    // console.log(this)
+    // console.log(this.$slots)
   }
 }
 </script>
---
+++ src/views/single-case/test-demo.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h3>这个test demo哈哈哈哈哈</h3>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'test-demo',
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+        mounted() {
+            console.log(this)
+            console.log(this.$slots)
+            console.log(this.$parent.$refs)
+            console.log(this.$parent.$options.name)
+            document.body.appendChild(this.$parent.$el)
+            console.log(this.$parent.$el)
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
